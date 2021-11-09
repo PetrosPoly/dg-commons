0ee357de70be887001b86b6804fb3edb19f6bdf9
Zelio Suter <suterz@ethz.ch>
2021-11-09 10:29:23 +0100

    add poisson

--- src/dg_commons_dev/state_estimators/utils.py
+++ src/dg_commons_dev/state_estimators/utils.py
@@ -19,7 +19,7 @@ class ExponentialParams(BaseParams):
 
 class Exponential:
     """
-    Exponential Distribution Parameters
+    Exponential Distribution
 
     f(t) = lambda * exp(- lambda * t) if t >= 0
     f(t) = 0 otherwise
@@ -48,6 +48,56 @@ class Exponential:
         assert t >= 0
 
         return self.params.lamb * math.exp(-self.params.lamb * t)
+
+
+@dataclass
+class PoissonParams(BaseParams):
+    """
+    Poisson Distribution Parameters
+    """
+
+    lamb: float = 1
+    """ lambda > 0 """
+
+    def __post_init__(self):
+        assert self.lamb > 0
+
+
+class Poisson:
+    """
+    Poisson Distribution
+
+    P(k Events) = lambda^k * exp(- lambda) / k!
+    """
+    REF_PARAMS: dataclass = PoissonParams
+
+    def __init__(self, params: ExponentialParams):
+        self.params = params
+
+    def pmf(self, k: int) -> float:
+        """
+        Probability mass function
+        @param k: number of events
+        @return: probability of k events
+        """
+        assert isinstance(k, int)
+
+        return math.pow(self.params.lamb, k) * math.exp(- self.params.lamb) / math.factorial(k)
+
+    def pdf(self, k: int) -> float:
+        """
+        Cumulative distribution function
+        @param k: number of events
+        @return: cumulative evaluation
+        """
+        k = int(k)
+
+        return_val = 0
+        for i in range(k):
+            return_val += self.pmf(i)
+        return_val += self.pmf(k)
+
+        return return_val
 
 
 PDistribution = Union[Exponential]
