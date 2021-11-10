dbdbb660f4a6cdebfb2e67d36f857b23ffa4cc71
Leon Zueger <leonzueger@bluewin.ch>
2021-11-10 14:05:47 +0100

    Added DynamicRoadGraph class and more functions for RoadGraph

--- src/dg_commons/maps/road_networks.py
+++ src/dg_commons/maps/road_networks.py
@@ -1,13 +1,18 @@
 from networkx import DiGraph, draw_networkx_edges, draw_networkx_nodes, draw_networkx_labels, all_simple_paths
 import networkx as nx
-from typing import List, Optional, Tuple, Mapping, Set
+from typing import List, Optional, Tuple, Mapping, Set, Dict, Any
 import numpy as np
 from commonroad.scenario.lanelet import LaneletNetwork
+from commonroad.planning.planning_problem import PlanningProblemSet
 from commonroad.common.file_reader import CommonRoadFileReader
+from commonroad.geometry.shape import Polygon  #question: or LaneletPolygon?
 import matplotlib.pyplot as plt
 from matplotlib.collections import PathCollection, LineCollection
+from dg_commons.sim import SimObservations, SimTime
+from dg_commons import PlayerName
 
 
+# currently not used
 def get_lanelet_center_coordinates(lanelet_network: LaneletNetwork, lanelet_id: int) -> np.ndarray:
     center = lanelet_network.find_lanelet_by_id(lanelet_id).polygon.center
     return center
@@ -71,10 +76,12 @@ class RoadGraph:
             if lanelet.lanelet_id in self.excluded_lanelets:
                 continue
             # add permissible lanelet to graph
-            center = get_lanelet_center_coordinates(lanelet_network, lanelet.lanelet_id)
+            polygon = lanelet_network.find_lanelet_by_id(lanelet.lanelet_id).polygon
+            #center = get_lanelet_center_coordinates(lanelet_network, lanelet.lanelet_id)
+
             self.road_graph.add_node(lanelet.lanelet_id,
                                      lanelet_type=lanelet.lanelet_type,
-                                     pos=center)
+                                     polygon=polygon)
 
             # add edge for all succeeding lanelets
             for id_successor in lanelet.successor:
@@ -115,16 +122,17 @@ class RoadGraph:
                 self.road_graph.add_edge(lanelet.lanelet_id, lanelet.adj_left, weight=weight)
         return
 
-    def plot_graph(self, filename: str, start_node: int, end_node: int) -> None:
+    def plot_graph(self, start_node: int, end_node: int, filename: str = None, ) -> None:
         plt.figure(1)
         plt.grid(True)
         plt.xlabel('X Axis')
         plt.ylabel('Y Axis')
         visited_nodes, special_edges = self.get_possible_resources(start_node=start_node, end_node=end_node)
-        goal_nodes = self.get_occupancy_children(occupancy_nodes=visited_nodes)
+        goal_nodes = self.get_occupancy_children(occupied_resources=visited_nodes)
         _, _, _ = self.get_collections_networkx(special_edges=special_edges, visited_nodes=visited_nodes,
                                                 goal_nodes=goal_nodes, start_node=start_node, end_node=end_node)
-        plt.savefig(filename)
+        if filename is not None:
+            plt.savefig(filename)
         plt.close()
 
     def get_collections_networkx(self, special_edges: Set[Tuple[int, int]] = None,
@@ -143,7 +151,7 @@ class RoadGraph:
         nodes = self.road_graph.nodes
         cents = []
         for node in nodes.data():
-            cents.append(node[-1]['pos'])
+            cents.append(node[-1]['polygon'].center) #tbd: works??
 
         centers = dict(zip(nodes.keys(), cents))
 
@@ -209,22 +217,55 @@ class RoadGraph:
             occupancy_edges |= set(path_edges)
         return occupancy_nodes, occupancy_edges
 
-    def get_occupancy_children(self, occupancy_nodes) -> List[int]:
+    def get_occupancy_children(self, occupied_resources: Set[int]) -> List[int]:
         """
-        Compute the children of the nodes in the "occupancy zone" as computed by "get_possible_occupancy"
+        Compute the children of the nodes in the "occupancy zone" as computed by "get_possible_resources"
 
-        :param occupancy_nodes: part of digraph where the ego could be on his journey to the goal
+        :param occupied_resources: part of digraph where the ego could be on his journey to the goal
         """
         children = []
-        for node in occupancy_nodes:
+        for node in occupied_resources:
             candidates = list(self.road_graph.successors(node))
             for cand in candidates:
-                if cand in occupancy_nodes:
+                if cand in occupied_resources:
                     continue
                 else:
                     children.append(cand)
 
         return children
+
+    def is_upstream(self, node_id: int, nodes: Set[int]) -> bool:
+        """
+        Determine wether a given node is upstream a set of nodes.
+        Returns True even if node_id is in nodes
+
+        :param node_id: node id of node we want to know if is upstream
+        :param nodes: set of nodes to check against
+        """
+
+        children = nx.traversal.bfs_tree(G=self.road_graph, source=node_id).nodes
+        is_upstream = (set(children) & nodes) != set()
+
+        return is_upstream
+
+    def shortest_path(self, start_node: int, end_node: int): #tbd: what type returned
+        paths = nx.all_shortest_paths(G=self.road_graph, source=start_node, target=end_node,
+                                      weight=None, method='dijkstra')
+        return paths
+
+    def get_lanelet_by_position(self, position: np.ndarray) -> int:
+        for lanelet_id, lanelet in self.road_graph.nodes.items(): #question: iterator working?
+            if lanelet['polygon'].contains_point(position):
+                return lanelet_id
+            else:
+                print("No lanelet found that contains the position you asked for.")
+
+    def set_node_attribute(self, attribute: str, value: Any, node: int) -> None:
+        try:
+            self.road_graph[node][attribute] = value
+        except KeyError:
+            print("Specified node does not exist.")
+
 
     # Following functions were written but are not currently used.
     # For now, keep for reference or reusage.
@@ -280,14 +321,84 @@ class RoadGraph:
         return self.return_circle(goal_center_coordinate,10)'''
 
 
+class DynamicRoadGraph(RoadGraph):
+    """
+        Class to represent dynamic digraphs.
+        Extend RoadGraph by adding positions of observed agents and start & end position of the ego
+    """
+    def __init__(self, lanelet_network: LaneletNetwork, excluded_lanelets: Optional[List[int]] = None):
+        """
+        """
+        super().__init__(lanelet_network=lanelet_network, excluded_lanelets=excluded_lanelets)
+        # question: will this initialize it if I write "DynamicRoadGraph(lanelet_network)" somewhere else?
+        # self.road_graph = road_graph
+        # self.planning_problem_set: PlanningProblemSet = planning_problem_set
+        self.ego_start: Dict[int, Any] = {}  # fixme: what type should Any be
+        self.ego_goal: Dict[int, Any] = {}  # fixme: what type should Any be
+        self.locations: Dict[PlayerName, List[Tuple[SimTime, int]]] = {k: [] for k in ['P1', 'Ego']} #fixme: make general
+
+    # get start and goal of ego
+    # question: why would we need more than one goal for the ego? Here keep only the first.
+    def _start_and_goal_info(self, planning_problem_set: PlanningProblemSet) -> None:
+        """
+        Get planning problems for ego vehicle from planning problem set of scenario.
+        Write planning problems as dictionary in self.ego_start and self.ego_goals.
+        If there is more than one goal, only keep first.
+        """
+        for problem_id, problem in planning_problem_set.planning_problem_dict.items():
+            self.ego_start[problem_id] = self.get_lanelet_by_position(problem.initial_state.position)
+            self.ego_goal[problem_id] = problem.goal.lanelets_of_goal_position[0]  # fixme: keep only first
+        return
+
+
+    """def get_start_and_goal_ego(self) -> Dict[int, Tuple[int, int]]:
+
+        planning_problems = {}
+        # todo: this needs to be validated
+        for problem_id, problem in self.planning_problem_set.planning_problem_dict.keys():
+            start: int = self.scenario.lanelet_network.find_lanelet_by_position(
+                problem.initial_state.position)[0][0]
+            goal: int = self.scenario.lanelet_network.find_lanelet_by_position(
+                problem.goal.state_list[0].position)[0][0]
+
+            planning_problems[problem_id] = tuple(start, goal)
+
+        return planning_problems"""
+
+    def update_locations(self, sim_obs: SimObservations):
+        time = sim_obs.time
+        for player, player_obs in sim_obs.players.items():
+            player_pos = np.array([player_obs.state.x, player_obs.state.y])  # leave out occupancy info for now. #todo: get position from X
+            # question: can go through player_obs directly?
+            lanelet_id = self.get_lanelet_by_position(player_pos)
+            self.locations[player].append((time, lanelet_id))
+        return
+
+    # update location in graph for all players. Need function def find_node_by_position
+    # fixme: could be made quicker by checking if node is the same as before and ending execution if this holds.
+    '''def update_locations(self, sim_obs: SimObservations):
+        for player, player_obs in sim_obs.players:
+            attribute = player + "_here"  # fixme: not optimal way to store
+            # reset previous positions
+            for node_id, node in self.road_graph.road_graph.nodes:
+                if node[attribute]:
+                    node[attribute] = False
+            player_pos = sim_obs.players[player].state.X  # leave out occupancy info for now. #todo: get position from X
+            # question: can go through player_obs?
+            lanelet_id = self.road_graph.get_lanelet_by_position(player_pos)
+
+            self.road_graph.set_node_attribute(attribute=attribute, value=True, node=lanelet_id)
+        return'''
+
+
 if __name__ == '__main__':
     scenario_path1 = "/home/leon/Documents/repos/driving-games/scenarios/commonroad-scenarios/scenarios/hand-crafted/ZAM_Zip-1_66_T-1.xml"  # remove e.g. 24
     scenario_path2 = "/home/leon/Documents/repos/driving-games/scenarios/commonroad-scenarios/scenarios/NGSIM/Peachtree/USA_Peach-1_1_T-1.xml"  # remove e.g. 52826
     scenario_path3 = "/home/leon/Documents/repos/driving-games/scenarios/commonroad-scenarios/scenarios/hand-crafted/DEU_Muc-1_2_T-1.xml"  # remove e.g. 24
 
-    scenario, planning_problem_set = CommonRoadFileReader(scenario_path3).open(lanelet_assignment=True)
+    scenario, planning_problem_sett = CommonRoadFileReader(scenario_path3).open(lanelet_assignment=True)
     net = scenario.lanelet_network
 
-    obj = RoadGraph(lanelet_network=net)
+    obj = RoadGraph(lanelet_network=net, planning_problem_set=planning_problem_set)
+    occupancy_nodes, _ = obj.get_possible_resources(start_node=3512, end_node=3450)
     obj.plot_graph(filename='graph_only', start_node=3512, end_node=3450)
-
