1959ba0cfb3e0f8fe2c86974f023f89f69b020b6
Leon Zueger <leonzueger@bluewin.ch>
2021-11-15 11:36:34 +0100

    Manipulate graph through attributes. Added graph update function.

--- src/dg_commons/maps/road_networks.py
+++ src/dg_commons/maps/road_networks.py
@@ -2,14 +2,16 @@ from networkx import DiGraph, draw_networkx_edges, draw_networkx_nodes, draw_net
 import networkx as nx
 from typing import List, Optional, Tuple, Mapping, Set, Dict, Any
 import numpy as np
-from commonroad.scenario.lanelet import LaneletNetwork
-from commonroad.planning.planning_problem import PlanningProblemSet
+from commonroad.scenario.lanelet import LaneletNetwork, Lanelet
+from commonroad.planning.planning_problem import PlanningProblem
+from commonroad.visualization.mp_renderer import MPRenderer
 from commonroad.common.file_reader import CommonRoadFileReader
-from commonroad.geometry.shape import Polygon  #question: or LaneletPolygon?
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 from matplotlib.collections import PathCollection, LineCollection
 from dg_commons.sim import SimObservations, SimTime
 from dg_commons import PlayerName
+from matplotlib.animation import FuncAnimation
 
 
 # currently not used
@@ -77,7 +79,7 @@ class RoadGraph:
                 continue
             # add permissible lanelet to graph
             polygon = lanelet_network.find_lanelet_by_id(lanelet.lanelet_id).polygon
-            #center = get_lanelet_center_coordinates(lanelet_network, lanelet.lanelet_id)
+            # center = get_lanelet_center_coordinates(lanelet_network, lanelet.lanelet_id)
 
             self.road_graph.add_node(lanelet.lanelet_id,
                                      lanelet_type=lanelet.lanelet_type,
@@ -120,26 +122,45 @@ class RoadGraph:
                                                   id_lanelet_1=lanelet.lanelet_id,
                                                   id_lanelet_2=lanelet.adj_left)
                 self.road_graph.add_edge(lanelet.lanelet_id, lanelet.adj_left, weight=weight)
+
+        self.set_default_attributes()
+
         return
 
-    def plot_graph(self, start_node: int, end_node: int, filename: str = None, ) -> None:
-        plt.figure(1)
-        plt.grid(True)
-        plt.xlabel('X Axis')
-        plt.ylabel('Y Axis')
-        visited_nodes, special_edges = self.get_possible_resources(start_node=start_node, end_node=end_node)
-        goal_nodes = self.get_occupancy_children(occupied_resources=visited_nodes)
-        _, _, _ = self.get_collections_networkx(special_edges=special_edges, visited_nodes=visited_nodes,
-                                                goal_nodes=goal_nodes, start_node=start_node, end_node=end_node)
-        if filename is not None:
-            plt.savefig(filename)
-        plt.close()
+    def set_default_attributes(self) -> None:
+        # set default attributes for nodes
+        for node in list(self.road_graph.nodes):
+            self.set_node_attribute(attribute='start', value=False, node=node)
+            self.set_node_attribute(attribute='goal', value=False, node=node)
+            self.set_node_attribute(attribute='ego_occupied_resource', value=False, node=node)
+            self.set_node_attribute(attribute='occupied_by_agent', value=False, node=node)
+            self.set_node_attribute(attribute='goal_of_interest', value=False, node=node)
 
-    def get_collections_networkx(self, special_edges: Set[Tuple[int, int]] = None,
+        # set default attributes for edges
+        for edge in list(self.road_graph.edges):
+            self.set_edge_attribute(attribute='ego_occupied_edge', value=False, edge=edge)
+        return
+
+    def plot_graph(self, file_path: str = None) -> None:
+        fig, ax = plt.subplots(figsize=(30, 30))
+        _, _, _ = self.get_collections_networkx(ax=ax)
+        if file_path is not None:
+            plt.savefig(file_path)
+        plt.close()
+        return
+
+    """def create_graph_animation(self, file_path: str, func, frames : int):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        anim = FuncAnimation(fig=fig, func=func, init_func=func, frames=frames, blit=True)#, frames=frame_count, blit=True, interval=dt
+        anim.save(filename=file_path)
+        return anim"""
+
+
+    """def get_collections_networkx(self, special_edges: Set[Tuple[int, int]] = None,
                                  visited_nodes: Set[int] = None, goal_nodes: List[int] = None,
                                  start_node: int = None, end_node: int = None) \
             -> Tuple[PathCollection, LineCollection, Mapping[int, plt.Text]]:
-        """
+        
         Get collections for plotting a graph on top of a scenario
 
         :param special_edges: Edges to color differently.
@@ -147,11 +168,11 @@ class RoadGraph:
         :param goal_nodes: possible other agents goals.
         :param start_node: Departure node of ego.
         :param end_node: Arrival node of ego.
-        """
+        
         nodes = self.road_graph.nodes
         cents = []
         for node in nodes.data():
-            cents.append(node[-1]['polygon'].center) #tbd: works??
+            cents.append(node[-1]['polygon'].center)  # tbd: works??
 
         centers = dict(zip(nodes.keys(), cents))
 
@@ -186,7 +207,7 @@ class RoadGraph:
 
         # return collections and set zorders
         # the functions draw_* already plot on axes
-        nodes_plot = draw_networkx_nodes(G=self.road_graph, pos=centers, node_size=500, node_color=node_colors)
+        nodes_plot = draw_networkx_nodes(G=self.road_graph, pos=centers, node_size=200, node_color=node_colors)
         nodes_plot.set_zorder(50)
 
         edges_plot = draw_networkx_edges(G=self.road_graph, pos=centers, edge_color=edge_colors)
@@ -195,6 +216,58 @@ class RoadGraph:
             edges_plot[edge] = edges_plot[edge].set_zorder(49)
         for label in labels_plot.keys():
             labels_plot[label].set_zorder(51)
+        return nodes_plot, edges_plot, labels_plot"""
+
+    def get_collections_networkx(self, ax: Axes = None) -> Tuple[PathCollection, LineCollection, Mapping[int, plt.Text]]:
+        """
+        Get collections for plotting a graph on top of a scenario
+
+        :param ax: Axes on which to draw the Artists
+        """
+        nodes = self.road_graph.nodes
+        edges = self.road_graph.edges
+        cents = []
+        for node in nodes.data():
+            cents.append(node[-1]['polygon'].center)
+
+        centers = dict(zip(nodes.keys(), cents))
+
+        # set default edge and node colors
+        edge_colors = ['k'] * len(self.road_graph.edges)
+        node_colors = ['#1f78b4'] * len(self.road_graph.nodes)
+
+        # set special node and edge colors depending on node and edge attributes
+        for node in nodes.data():
+            node_idx = list(nodes).index(node[0])
+            if node[1]['goal']:
+                node_colors[node_idx] = 'limegreen'
+            if node[1]['start']:
+                node_colors[node_idx] = 'gold'
+            if node[1]['goal_of_interest']:
+                node_colors[node_idx] = 'magenta'
+            if node[1]['ego_occupied_resource']:
+                node_colors[node_idx] = 'r'
+            if node[1]['occupied_by_agent']:
+                node_colors[node_idx] = 'cyan'
+
+        for edge in edges.data():
+            edge_idx = list(edges).index((edge[0], edge[1]))
+            if edge[2]['ego_occupied_edge']:
+                edge_colors[edge_idx] = 'r'
+
+        # return collections and set zorders
+        # the functions draw_* already plots on axes
+        nodes_plot = draw_networkx_nodes(G=self.road_graph, ax=ax, pos=centers, node_size=200, node_color=node_colors)
+        nodes_plot.set_zorder(50)
+
+        edges_plot = draw_networkx_edges(G=self.road_graph, ax=ax, pos=centers, edge_color=edge_colors)
+        for edge in range(len(edges_plot)):
+            edges_plot[edge].set_zorder(49)
+
+        labels_plot = draw_networkx_labels(G=self.road_graph, ax=ax, pos=centers)
+        for label in labels_plot.keys():
+            labels_plot[label].set_zorder(51)
+
         return nodes_plot, edges_plot, labels_plot
 
     def get_possible_resources(self, start_node: int, end_node: int) -> Tuple[Set[int], Set[Tuple[int, int]]]:
@@ -248,24 +321,29 @@ class RoadGraph:
 
         return is_upstream
 
-    def shortest_path(self, start_node: int, end_node: int): #tbd: what type returned
+    def shortest_path(self, start_node: int, end_node: int):  # tbd: what type returned
         paths = nx.all_shortest_paths(G=self.road_graph, source=start_node, target=end_node,
                                       weight=None, method='dijkstra')
         return paths
 
     def get_lanelet_by_position(self, position: np.ndarray) -> int:
-        for lanelet_id, lanelet in self.road_graph.nodes.items(): #question: iterator working?
-            if lanelet['polygon'].contains_point(position):
+        for lanelet_id, lanelet_polygon in list(self.road_graph.nodes(data='polygon')):
+            if lanelet_polygon.contains_point(position):
                 return lanelet_id
-            else:
-                print("No lanelet found that contains the position you asked for.")
+
+        print("Position: " + str(position) + ". No lanelet found that contains the position you asked for.")
 
     def set_node_attribute(self, attribute: str, value: Any, node: int) -> None:
         try:
-            self.road_graph[node][attribute] = value
+            self.road_graph.nodes[node][attribute] = value
         except KeyError:
             print("Specified node does not exist.")
 
+    def set_edge_attribute(self, attribute: str, value: Any, edge: Tuple[int,int]) -> None:
+        try:
+            self.road_graph.edges[(edge[0], edge[1])][attribute] = value
+        except KeyError:
+            print("Specified edge does not exist.")
 
     # Following functions were written but are not currently used.
     # For now, keep for reference or reusage.
@@ -326,30 +404,68 @@ class DynamicRoadGraph(RoadGraph):
         Class to represent dynamic digraphs.
         Extend RoadGraph by adding positions of observed agents and start & end position of the ego
     """
+
     def __init__(self, lanelet_network: LaneletNetwork, excluded_lanelets: Optional[List[int]] = None):
         """
         """
         super().__init__(lanelet_network=lanelet_network, excluded_lanelets=excluded_lanelets)
-        # question: will this initialize it if I write "DynamicRoadGraph(lanelet_network)" somewhere else?
-        # self.road_graph = road_graph
-        # self.planning_problem_set: PlanningProblemSet = planning_problem_set
-        self.ego_start: Dict[int, Any] = {}  # fixme: what type should Any be
-        self.ego_goal: Dict[int, Any] = {}  # fixme: what type should Any be
-        self.locations: Dict[PlayerName, List[Tuple[SimTime, int]]] = {k: [] for k in ['P1', 'Ego']} #fixme: make general
+        # self.ego_start: Dict[int, Any] = {}  # fixme: what type should Any be
+        # self.ego_goal: Dict[int, Any] = {}  # fixme: what type should Any be
+        self.ego_start: Optional[int] = None
+        self.ego_goal: Optional[int] = None
+        self.ego_problem_updated: bool = False
+        self.locations: Dict[PlayerName, List[Tuple[SimTime, int]]] = {k: [] for k in
+                                                                       ['P1', 'Ego']}  # fixme: make general
 
     # get start and goal of ego
     # question: why would we need more than one goal for the ego? Here keep only the first.
-    def _start_and_goal_info(self, planning_problem_set: PlanningProblemSet) -> None:
-        """
+    # tbd: may only use one problem instead of a set
+    #tbd: currently called in __init__ of PredAgent.
+    # Should be called somewhere else in case goals are updated?
+    def start_and_goal_info(self, problem: PlanningProblem) -> None:
+        """Get start an goal for ego vehicle a from planning problem of scenario.
+        Write planning problem in self.ego_start and self.ego_goals.
+        If there is more than one goal in planning problem, only keep first."""
+        start = self.get_lanelet_by_position(problem.initial_state.position)
+        self.ego_start = start
+        goal = problem.goal.lanelets_of_goal_position
+        if goal is not None:
+            self.ego_goal = goal[0][0]  # fixme: keep only first
+        else:
+            next_node = list(self.road_graph.successors(start))
+            self.ego_goal = next_node[0]  # fixme: only works if there is a node. Nonsense, just to make it run.
+            print("No goal in planning problem set. Goal is set to last lanelet "
+                  "after merging successor of start lanelet.")  # fixme: comment is what should be done
+        print("Start of planning problem: ")
+        print(self.ego_start)
+        print("Goal of planning problem: ")
+        print(self.ego_goal)
+
+        # there is a new plan
+        self.ego_problem_updated = True
+        return
+    """def start_and_goal_info(self, problem: PlanningProblem) -> None:
         Get planning problems for ego vehicle from planning problem set of scenario.
         Write planning problems as dictionary in self.ego_start and self.ego_goals.
         If there is more than one goal, only keep first.
-        """
+        
         for problem_id, problem in planning_problem_set.planning_problem_dict.items():
-            self.ego_start[problem_id] = self.get_lanelet_by_position(problem.initial_state.position)
-            self.ego_goal[problem_id] = problem.goal.lanelets_of_goal_position[0]  # fixme: keep only first
-        return
-
+            start = self.get_lanelet_by_position(problem.initial_state.position)
+            self.ego_start[problem_id] = start
+            goal = problem.goal.lanelets_of_goal_position
+            if goal is not None:
+                self.ego_goal[problem_id] = goal[0][0]  # fixme: keep only first
+            else:
+                next_node = list(self.road_graph.successors(start))
+                self.ego_goal[problem_id] = next_node[
+                    0]  # fixme: only works if there is a node. Nonsense, just to make it run.
+                print("No goal in planning problem set. Goal is set to last lanelet "
+                      "after merging successor of start lanelet.") #fixme: comment is what should be done
+            print("Start of problem with id " + str(problem_id)+":")
+            print(self.ego_start[problem_id])
+            print("Goal of problem with id " + str(problem_id) + ":")
+            print(self.ego_goal[problem_id])
+        return"""
 
     """def get_start_and_goal_ego(self) -> Dict[int, Tuple[int, int]]:
 
@@ -368,7 +484,8 @@ class DynamicRoadGraph(RoadGraph):
     def update_locations(self, sim_obs: SimObservations):
         time = sim_obs.time
         for player, player_obs in sim_obs.players.items():
-            player_pos = np.array([player_obs.state.x, player_obs.state.y])  # leave out occupancy info for now. #todo: get position from X
+            player_pos = np.array([player_obs.state.x,
+                                   player_obs.state.y])  # leave out occupancy info for now. #todo: get position from X
             # question: can go through player_obs directly?
             lanelet_id = self.get_lanelet_by_position(player_pos)
             self.locations[player].append((time, lanelet_id))
@@ -389,6 +506,45 @@ class DynamicRoadGraph(RoadGraph):
 
             self.road_graph.set_node_attribute(attribute=attribute, value=True, node=lanelet_id)
         return'''
+
+    def update_dynamic_graph(self) -> None:
+        """
+        Update node attributes depending on ego_start, ego_goal and locations of all agents
+        """
+
+        if self.ego_problem_updated:
+            # reset default values of node and edge attributes
+            self.set_default_attributes()
+
+            # compute new edge attributes
+            self.set_node_attribute(attribute='start', value=True, node=self.ego_start)
+            self.set_node_attribute(attribute='goal', value=True, node=self.ego_goal)
+
+            ego_resources_nodes, ego_resources_edges = \
+                self.get_possible_resources(start_node=self.ego_start, end_node=self.ego_goal)
+            goals_of_interest = self.get_occupancy_children(ego_resources_nodes)
+
+            for node in ego_resources_nodes:
+                self.set_node_attribute(attribute='ego_occupied_resource', value=True, node=node)
+            for edge in ego_resources_edges:
+                self.set_edge_attribute(attribute='ego_occupied_edge', value=True, edge=edge)
+            for node in goals_of_interest:
+                self.set_node_attribute(attribute='goal_of_interest', value=True, node=node)
+
+        for player, locations in self.locations.items():
+            if player != 'Ego':
+                # skip first loop iteration
+                if len(locations) > 1:
+                    # remove previous location
+                    self.set_node_attribute(attribute='occupied_by_agent', value=False, node=locations[-2][1])
+                    # set new location
+                self.set_node_attribute(attribute='occupied_by_agent', value=player, node=locations[-1][1])
+
+        # updated plan has been processed
+        self.ego_problem_updated = False
+
+
+
 
 
 if __name__ == '__main__':
